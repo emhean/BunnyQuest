e90ceed5267778eaaa28f5c727471049ddca9625
Emily Hedman <12emhean@gmail.com>
2020-01-06 13:07:21 +0100

    added comps

---
+++ BunnyQuest/ECS/Components/CmpAnim.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyQuest.ECS.Components
+{
+    class CmpAnim : Component
+    {
+    }
+}
