1c6062f99cab58918159f75c022e2ebf41ef30dd
Otto Tysklind <ottotysklind@hotmail.com>
2020-01-06 12:26:58 +0100

    Added ECS

---
+++ BunnyQuest/ECS/CompPos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyQuest.ECS
+{
+    class CompPos
+    {
+        float pos_x = 0;
+        float pos_y = 0;
+        string coord = [pos_x, pos_y];
+
+
+        
+        public string Get_pos()
+            return coord;
+        
+
+    }
+}
