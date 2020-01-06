33d431d07b789b5ff5eff3d16d9d4a8e771ad2d4
Otto Tysklind <ottotysklind@hotmail.com>
2020-01-06 14:43:42 +0100

    Finished collisison prototype

--- BunnyQuest/ECS/System.cs
+++ BunnyQuest/ECS/System.cs
@@ -82,7 +82,33 @@ namespace BunnyQuest.ECS
 
                         if (c2 != null)
                         {
-                            
+                            float Ax1 = c1.rect.X;
+                            float Ax2 = Ax1 + c1.rect.Width;
+                            float Bx1 = c2.rect.X;
+                            float Bx2 = Bx1 + c2.rect.Width;
+
+                            if( ( Ax1 < Bx1 && Bx1 < Ax2 )
+                                || (Ax1 < Bx2 && Bx2 < Ax2))
+                            {
+                                float Ay1 = c1.rect.Y;
+                                float Ay2 = Ay1 + c1.rect.Height;
+                                float By1 = c2.rect.Y;
+                                float By2 = By1 + c2.rect.Height;
+
+                                if ((Ay1 < By1 && By1 <Ay2)
+                                || (Ay1 < By2 && By2 < Ay2))
+                                {
+                                    if (Math.Abs(Bx1 - Ax1) < Math.Abs(Bx1 - Ax2))
+                                        Ax2 = Bx1;
+                                    else
+                                        Ax1 = Bx2;
+
+                                    if (Math.Abs(By1 - Ay1) < Math.Abs(By1 - Ay2))
+                                        Ay1 = By2;
+                                    else
+                                        Ay2 = By1;
+                                }
+                            }
                         }
                     }
                 }
