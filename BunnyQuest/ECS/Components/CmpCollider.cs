85ba8d1905f416555e0a2606cef36ae41c9de61b
Otto Tysklind <ottotysklind@hotmail.com>
2020-01-25 17:42:21 +0100

    new file transform matrix

--- BunnyQuest/ECS/Components/CmpCollider.cs
+++ BunnyQuest/ECS/Components/CmpCollider.cs
@@ -36,11 +36,6 @@ namespace BunnyQuest.ECS.Components
 
         public void SetPosition(int x, int y)
         {
-            if (x < 0)
-                x = 0;
-            if (y < 0)
-                y = 0;
-
             entity.pos.X = x;
             entity.pos.Y = y;
 
---
+++ BunnyQuest/Entities/Testa Transform.cs
@@ -0,0 +1,37 @@
+﻿using BunnyQuest.ECS;
+using BunnyQuest.ECS.Components;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+class MatrixFunktioner
+{
+    public int[][] Matrix_transformer(int[][] matrix)
+    {
+        var width = matrix.Length;
+        int height = matrix[0].Length;
+        int[][] new_matrix = new int[width][];
+
+        for (int w = 0; w < new_matrix.Length; w++)
+            for (int h = 0; h < height; h++)
+                new_matrix[w] = new int[height];
+
+
+        if (height == width)
+        {
+            for (int x = width; x >= 0; x--)
+            {
+                for (int y = height; y >= 0; y--)
+                {
+
+                    new_matrix[height - y][x] = matrix[x][y];
+                }
+            }
+            return new_matrix;
+        }
+        else throw new System.Exception("Your 2D array is not square! So we crash.");
+
+    }
+}
+
