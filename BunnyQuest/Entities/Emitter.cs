662044441b1cd5715b75fa603bbfb745ee65f86a
Otto Tysklind <ottotysklind@hotmail.com>
2020-01-26 21:23:17 +0100

    added basic entity emitter

---
+++ BunnyQuest/Entities/Emitter.cs
@@ -0,0 +1,16 @@
+﻿using BunnyQuest.ECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyQuest.Entities
+{
+    class Emitter : Entity
+    {
+        public Emitter(int UUID) : base(UUID)
+        {
+        }
+    }
+}
